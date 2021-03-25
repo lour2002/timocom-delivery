d8b80f7f442d9e2db70089b5e60e4d9a32abea79
lour2002 <boris.avs@live.ru>
2021-03-25 08:34:47 +0200

    Save progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
+    <x-container class="mx-auto max-w-screen-lg">
+        <x-search-task>
+        </x-search-task>
+    </x-container>
+    <x-modal-search-setting>
 
+    </x-modal-search-setting>
 </x-app-layout>
