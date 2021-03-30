968e4fc950f75d4f2976b02d325c4a13030db844
Sergey <triongroup@gmail.com>
2021-03-30 23:23:33 +0300

    added fields to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,7 +10,9 @@ class Task extends Model
     protected $fillable = [
         'name', 'status_job', 'version_task', 'fromSelectOpt', 'as_country', 'as_zip',
         'as_radius', 'toSelectOpt', 'toSelectOptArray', 'freightSelectOpt', 'length_min', 'length_max',
-        'weight_min', 'weight_max', 'dateSelectOpt', 'individual_days', 'period_start', 'period_stop'
+        'weight_min', 'weight_max', 'dateSelectOpt', 'individual_days', 'period_start', 'period_stop',
+        'car_country', 'car_zip', 'car_city', 'car_price_empty', 'car_price', 'car_price_extra_points',
+        'exchange_equipment', 'minimal_price_order', 'percent_stop_value', 'tags', 'email_template'
     ];
 
 
