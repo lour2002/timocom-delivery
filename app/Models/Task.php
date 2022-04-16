4634a01e7d9a809b5c3de1a7624dfa6677f61d83
boris.avsiukevych <boris.avs@live.ru>
2022-04-16 17:30:11 +0300

    [v2] task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,19 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
+use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
 
 class Task extends Model
 {
+    use AsSource, Filterable, Attachable;
+
     public const STATUS_START = '3';
     public const STATUS_TEST = '2';
     public const STATUS_STOP = '1';
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
     protected $fillable = [
         'user_id','user_key', 'name', 'status_job', 'num', 'version_task', 'fromSelectOpt', 'as_country', 'as_zip',
         'as_radius', 'toSelectOpt', 'toSelectOptArray', 'freightSelectOpt', 'length_min', 'length_max',
-        'weight_min', 'weight_max', 'dateSelectOpt', 'individual_days', 'period_start', 'period_stop',
-        'car_country', 'car_zip', 'car_city', 'car_price_empty', 'car_price', 'car_price_extra_points',
-        'exchange_equipment', 'minimal_price_order', 'percent_stop_value', 'cross_border', 'tags', 'email_template'
+        'weight_min', 'weight_max', 'dateSelectOpt', 'individual_days', 'car_position_coordinates','car_price_empty',
+        'car_price', 'car_price_extra_points', 'car_price_special_price', 'exchange_equipment', 'minimal_price_order',
+        'percent_stop_value', 'cross_border', 'tags', 'email_template'
     ];
 }
---
+++ database/migrations/2022_04_16_094132_create_timocom_tasks_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimocomTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('user_key');
+            $table->string('name')->default('Task');
+            $table->char('status_job', 1)->default('0');
+            $table->tinyInteger('num');
+            $table->tinyInteger('version_task')->default(0);
+            $table->string('fromSelectOpt',100)->nullable();
+            $table->string('as_country',100)->nullable();
+            $table->string('as_zip', 10)->nullable();
+            $table->char('as_radius',5)->nullable();
+            $table->string('toSelectOpt',100)->nullable();
+            $table->text('toSelectOptArray')->nullable();
+            $table->string('freightSelectOpt',100)->nullable();
+            $table->string('length_min',50)->nullable();
+            $table->string('length_max',50)->nullable();
+            $table->string('weight_min',50)->nullable();
+            $table->string('weight_max',50)->nullable();
+            $table->string('dateSelectOpt',100)->nullable();
+            $table->string('individual_days',50)->nullable();
+            $table->string('car_position_coordinates',50)->nullable();
+            $table->double('car_price_empty', 8, 2)->nullable();
+            $table->double('car_price', 8, 2)->nullable();
+            $table->double('car_price_extra_points', 8, 2)->nullable();
+            $table->text('car_price_special_price')->nullable();
+            $table->tinyInteger('exchange_equipment')->default(0)->nullable();
+            $table->double('minimal_price_order',8,2)->nullable();
+            $table->integer('percent_stop_value')->nullable();
+            $table->text('cross_border')->nullable();
+            $table->text('tags')->nullable();
+            $table->text('email_template')->nullable();
+
+            $table->index(['user_id','user_key','status_job','num']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
