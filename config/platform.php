f3ecc1ee95eaec966bacdb3cb4711a5450b04577
boris.avsiukevych <boris.avs@live.ru>
2022-10-03 11:47:09 +0300

    [PLATFORM] added manifest for PWA

--- config/platform.php
+++ config/platform.php
@@ -129,7 +129,7 @@ return [
 
     'template' => [
         'header' => null,
-        'footer' => null,
+        'footer' => 'brand.footer',
     ],
 
     /*
---
+++ resources/views/brand/footer.blade.php
@@ -0,0 +1,3 @@
+@push('head')
+    <link rel="manifest" href="/site.webmanifest?v=1">
+@endpush
